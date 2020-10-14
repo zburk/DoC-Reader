75870ac8f6663c51b96f0cf1f7b0b120f14bd558
Zachary Burk <zachburk7@gmail.com>
2020-10-14 09:54:15 -0400

    Initial Commit

---
+++ DoC Reader/DoC_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoC_ReaderApp.swift
+//  DoC Reader
+//
+//  Created by Zachary Joseph Shyi-An Burk on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct DoC_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
